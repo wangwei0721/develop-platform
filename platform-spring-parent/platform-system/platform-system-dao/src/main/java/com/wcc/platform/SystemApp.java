1a9b7043b7ea0f97d7cae7d7d8d8f84bb30fe60f
wangwei <wangwei_0721@163.com>
2022-01-06 20:44:53 +0800

    config-server向eureka-server注册

--- platform-spring-parent/platform-system/platform-system-dao/src/main/java/com/wcc/platform/SystemApp.java
+++ platform-spring-parent/platform-system/platform-system-dao/src/main/java/com/wcc/platform/SystemApp.java
@@ -2,10 +2,8 @@ package com.wcc.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class SystemApp {
 
     public static void main(String[] args) {
